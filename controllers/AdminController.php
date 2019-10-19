094cfb4b7693a37eaa56584337eb89a5ef9fb46f
Hamizi <hamizi@ums.edu.my>
2019-10-19 21:31:00 +0800

    add memory limit for data extraction

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -52,6 +52,8 @@ class AdminController extends \yii\web\Controller {
 
     public function actionData() {
         
+        ini_set('memory_limit', '1024M'); // or you could use 1G
+        
         $searchModel = new OkuMainSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
