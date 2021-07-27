89d4b2c970f6a4d71ffffed15ae042afa54d1aba
hamizi <hamizi@ums.edu.my>
2021-07-27 19:56:23 +0800

    add new admin bfi

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-mini">MISI</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
