f2bc32e7d84fd4a38ab7545ef61613bacb473058
hamizi <hamizi@ums.edu.my>
2022-07-29 08:36:07 +0800

    new dashboard

--- views/site/dashboard.php
+++ views/site/dashboard.php
@@ -3,180 +3,187 @@
 use yii\helpers\Html;
 ?>
 
+<div class="box box-info">
+    <div class="box-header with-border">
+        <h3 class="box-title"><i class="fa fa-th-large"></i>&nbsp;<strong>Laman Utama</strong></h3>
+    </div>
+    <div class="box-body">
+        <p>Setiap individu mempunyai pengalaman dengan ujian. Ujian adalah alat pengukuran atau teknik yang digunakan untuk mengukur tingkah laku atau membantu dalam pemahaman dan ramalan tingkah laku. Ujian psikologi adalah sekumpulan item yang dibentuk untuk mengukur ciri-ciri manusia yang berkaitan dengan proses kognitif dan tingkah laku. Ujian merupakan salah satu elemen penting dalam psikologi dan ia bergantung pada pengetahuan yang diperoleh dalam kajian penyelidikan psikologi. Kesahan dan kebolehpercayaan pengukuran penting kepada penyelidikan psikologi. Dalam masyarakat yang kompleks sekarang ini, prinsip yang relevan, aplikasi, dan peranan yang dimainkan oleh ujian psikologi melangkaui bidang psikologi.
+
+        <p>Memandangkan ujian psikologi dilihat signifikan, maka penyelidik telah berusaha menjadikan alat ujian psikologi sama ada yang telah dibina sendiri oleh penyelidik atau menggunakan alat ujian yang sedia ada dibentuk dalam bentuk digital. Terdapat enam alat ujian atau instrumen yang ada dalam Portal MISI iaitu:
+
+        <p>
+        <p>
+        <ol>
+            <li>KebahagiaanKu-OKU/MyHappinesspPwD</li>
+            <li>Malay Version of Emblematic Analysis (EA-Malay)</li>
+            <li>Malay Version of Emblematic Analysis (EA-Malay) v2</li>
+            <li>Ten-Item Personality Inventory-Malay (TIPI-Malay)</li>
+            <li>The HEXACO Personality Inventory-Malay (HEXACO-Malay)</li>
+            <li>Big Five Inventory-10-Malay (BFI-Malay)</li>
+        </ol>
+        </p>
 
 
-<!-- /.col -->
-<div class="col-md-12 col-xs-12 col-lg-12">
-    <div class="box box-primary">
-        <div class="box-header with-border">
-            <h3 class="box-title">Laman Utama</h3>
+        <p>
+            Keenam-enam alat ujian atau instrumen ini boleh diakses di pautan berikut, <a href="https://uppsiks.ums.edu.my/">https://uppsiks.ums.edu.my</a>. Inventori secara atas talian ini telah dibangunkan sendiri oleh Muhammad Idris Bullare @ Bahari (2020) hasil daripada kajian di peringkat PhD dan ia dinamakan sebagai e-Instrumen Kesejahteraan Subjektif Orang Kurang Upaya-Fizikal (e-IKSOKU-F), dan kemudiannya ditukar menjadi KebahagiaanKu-OKU (KK-OKU)/MyHappiness-PwD/MH-PwD), manakala instrument EA-Malay, Ten-Item Personality Inventory-Malay (TIPI-Malay), The HEXACO Personality Inventory-Malay (HEXACO-Malay) dan Big Five Inventory-10-Malay (BFI-Malay) merupakan instrumen piawai yang telah melalui proses terjemahan dan mendapat kebenaran daripada pengasas bagi inventori tersebut.
+        <p>
+            Akhir kata, saya ingin mengambil kesempatan mengucapkan ribuan terima kasih kepada pengguna yang sudi mengunjungi portal MISI ini dan seterusnya menjawab alat ujian atau inventori yang disediakan.
+        <p>
+            Profesor Madya Dr. Muhammad Idris Bin Bullare @ Bahari<br>
+            Unit Penyelidikan Psikologi dan Kesihatan Sosial<br>
+            Fakulti Psikologi dan Pendidikan<br>
+            Universiti Malaysia Sabah<br>
+            Tel: +6088-320 000 Samb: 8155<br>
+            Bimbit: +60165202070<br>
+            Faks: +6088-320 440<br>
+            Emel: ferlis@ums.edu.my<br>
+        </p>
 
-            <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-            </div>
-            <!-- /.box-tools -->
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-            <p>Setiap individu mempunyai pengalaman dengan ujian. Ujian adalah alat pengukuran atau teknik yang digunakan untuk mengukur tingkah laku atau membantu dalam pemahaman dan ramalan tingkah laku. Ujian psikologi adalah sekumpulan item yang dibentuk untuk mengukur ciri-ciri manusia yang berkaitan dengan proses kognitif dan tingkah laku. Ujian merupakan salah satu elemen penting dalam psikologi dan ia bergantung pada pengetahuan yang diperoleh dalam kajian penyelidikan psikologi. Kesahan dan kebolehpercayaan pengukuran penting kepada penyelidikan psikologi. Dalam masyarakat yang kompleks sekarang ini, prinsip yang relevan, aplikasi, dan peranan yang dimainkan oleh ujian psikologi melangkaui bidang psikologi.
+        <div class="col-md-6 col-xs-12 col-lg-4">
 
-            <p>Memandangkan ujian psikologi dilihat signifikan, maka penyelidik telah berusaha menjadikan alat ujian psikologi sama ada yang telah dibina sendiri oleh penyelidik atau menggunakan alat ujian yang sedia ada dibentuk dalam bentuk digital. Terdapat enam alat ujian atau instrumen yang ada dalam Portal MISI iaitu:
+            <div class="box box-primary box-solid">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="fa fa-wheelchair">&nbsp;</i>KK-OKU</h3>
 
-            <p>
-            <ol>
-                <li>KebahagiaanKu-OKU/MyHappinesspPwD</li>
-                <li>Malay Version of Emblematic Analysis (EA-Malay)</li>
-                <li>Malay Version of Emblematic Analysis (EA-Malay) v2</li>
-                <li>Ten-Item Personality Inventory-Malay (TIPI-Malay)</li>
-                <li>The HEXACO Personality Inventory-Malay (HEXACO-Malay)</li>
-                <li>Big Five Inventory-10-Malay (BFI-Malay)</li>
-            </ol>
-
-
-            <p>
-                Keenam-enam alat ujian atau instrumen ini boleh diakses di pautan berikut, <a href="https://uppsiks.ums.edu.my/">https://uppsiks.ums.edu.my</a>. Inventori secara atas talian ini telah dibangunkan sendiri oleh Muhammad Idris Bullare @ Bahari (2020) hasil daripada kajian di peringkat PhD dan ia dinamakan sebagai e-Instrumen Kesejahteraan Subjektif Orang Kurang Upaya-Fizikal (e-IKSOKU-F), dan kemudiannya ditukar menjadi KebahagiaanKu-OKU (KK-OKU)/MyHappiness-PwD/MH-PwD), manakala instrument EA-Malay, Ten-Item Personality Inventory-Malay (TIPI-Malay), The HEXACO Personality Inventory-Malay (HEXACO-Malay) dan Big Five Inventory-10-Malay (BFI-Malay) merupakan instrumen piawai yang telah melalui proses terjemahan dan mendapat kebenaran daripada pengasas bagi inventori tersebut.
-            <p>
-                Akhir kata, saya ingin mengambil kesempatan mengucapkan ribuan terima kasih kepada pengguna yang sudi mengunjungi portal MISI ini dan seterusnya menjawab alat ujian atau inventori yang disediakan.
-            <p>
-                Profesor Madya Dr. Muhammad Idris Bin Bullare @ Bahari<br>
-                Unit Penyelidikan Psikologi dan Kesihatan Sosial<br>
-                Fakulti Psikologi dan Pendidikan<br>
-                Universiti Malaysia Sabah<br>
-                Tel: +6088-320 000 Samb: 8155<br>
-                Bimbit: +60165202070<br>
-                Faks: +6088-320 440<br>
-                Emel: ferlis@ums.edu.my<br>
-
-
-            <div class="col-md-4">
-                <div class="box box-primary box-solid">
-                    <div class="box-header with-border">
-                        <h3 class="box-title"><i class="fa fa-wheelchair">&nbsp;</i>KK-OKU</h3>
-
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                        </div>
-                        <!-- /.box-tools -->
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
                     </div>
-                    <!-- /.box-header -->
-                    <div class="box-body" style="">
-                        <p>KebahagiaanKu-OKU/MyHappiness-PwD</p>
-                        <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['iksokuf/index'], ['class' => 'small-box-footer']) ?>
-                    </div>
-                    <!-- /.box-body -->
-
+                    <!-- /.box-tools -->
                 </div>
-                <!-- /.box -->
-            </div>
-
-            <div class="col-md-4">
-                <div class="box box-warning box-solid">
-                    <div class="box-header with-border">
-                        <h3 class="box-title"><i class="fa fa-line-chart">&nbsp;</i>EA-MALAY</h3>
-
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                        </div>
-                        <!-- /.box-tools -->
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body" style="">
-                        <p>Malay Version of Emblematic Analysis</p>
-                        <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['mea/index'], ['class' => 'small-box-footer']) ?>
-                    </div>
-                    <!-- /.box-body -->
-
+                <!-- /.box-header -->
+                <div class="box-body" style="">
+                    <p>KebahagiaanKu-OKU/MyHappiness-PwD</p>
+                    <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['iksokuf/index'], ['class' => 'small-box-footer']) ?>
                 </div>
-                <!-- /.box -->
-            </div>
+                <!-- /.box-body -->
 
-            <div class="col-md-4">
-                <div class="box box-default box-solid">
-                    <div class="box-header with-border">
-                        <h3 class="box-title"><i class="fa fa-bar-chart">&nbsp;</i>EA-MALAY (v2)</h3>
-
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                        </div>
-                        <!-- /.box-tools -->
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body" style="">
-                        <p>Malay Version of Emblematic Analysis Version 2</p>
-                        <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['mea-two/index'], ['class' => 'small-box-footer']) ?>
-                    </div>
-                    <!-- /.box-body -->
-
-                </div>
-                <!-- /.box -->
-            </div>
-
-            <div class="col-md-4">
-                <div class="box box-danger box-solid">
-                    <div class="box-header with-border">
-                        <h3 class="box-title"><i class="fa fa-bar-chart">&nbsp;</i>TIPI-MALAY</h3>
-
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                        </div>
-                        <!-- /.box-tools -->
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body" style="">
-                        <p>Ten-Item Personality Inventory-Malay (TIPI-Malay)</p>
-                        <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['tipi/index'], ['class' => 'small-box-footer']) ?>
-                    </div>
-                    <!-- /.box-body -->
-
-                </div>
-            </div>
-
-            <div class="col-md-4">
-                <div class="box box-success box-solid">
-                    <div class="box-header with-border">
-                        <h3 class="box-title"><i class="fa fa-user-circle-o">&nbsp;</i>HEXACO-MALAY</h3>
-
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                        </div>
-                        <!-- /.box-tools -->
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body" style="">
-                        <p>The HEXACO Personality Inventory-Malay (HEXACO-Malay)</p>
-                        <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['hexaco/index'], ['class' => 'small-box-footer']) ?>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="box box-default box-solid">
-                    <div class="box-header with-border bg-maroon">
-                        <h3 class="box-title"><i class="fa fa-user-secret">&nbsp;</i>BFI-MALAY</h3>
-
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                        </div>
-                        <!-- /.box-tools -->
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body" style="">
-                        <p>Big Five Inventory-10-Malay (BFI-Malay)</p>
-                        <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['bfi/index'], ['class' => 'small-box-footer']) ?>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
             </div>
         </div>
-        <!-- /.box-body -->
+
+        <div class="col-md-6 col-xs-12 col-lg-4">
+            <div class="box box-warning box-solid">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="fa fa-line-chart">&nbsp;</i>EA-MALAY</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body" style="">
+                    <p>Malay Version of Emblematic Analysis</p>
+                    <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['mea/index'], ['class' => 'small-box-footer']) ?>
+                </div>
+                <!-- /.box-body -->
+
+            </div>
+        </div>
+
+        <div class="col-md-6 col-xs-12 col-lg-4">
+            <div class="box box-default box-solid">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="fa fa-bar-chart">&nbsp;</i>EA-MALAY (v2)</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body" style="">
+                    <p>Malay Version of Emblematic Analysis Version 2</p>
+                    <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['mea-two/index'], ['class' => 'small-box-footer']) ?>
+                </div>
+                <!-- /.box-body -->
+
+            </div>
+        </div>
+
+        <div class="col-md-6 col-xs-12 col-lg-4">
+            <div class="box box-danger box-solid">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="fa fa-bar-chart">&nbsp;</i>TIPI-MALAY</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body" style="">
+                    <p>Ten-Item Personality Inventory-Malay (TIPI-Malay)</p>
+                    <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['tipi/index'], ['class' => 'small-box-footer']) ?>
+                </div>
+                <!-- /.box-body -->
+
+            </div>
+        </div>
+        <div class="col-md-6 col-xs-12 col-lg-4">
+            <div class="box box-success box-solid">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="fa fa-user-circle-o">&nbsp;</i>HEXACO-MALAY</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body" style="">
+                    <p>The HEXACO Personality Inventory-Malay (HEXACO-Malay)</p>
+                    <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['hexaco/index'], ['class' => 'small-box-footer']) ?>
+                </div>
+                <!-- /.box-body -->
+            </div>
+        </div>
+
+        <div class="col-md-6 col-xs-12 col-lg-4">
+            <div class="box box-primary box-solid">
+                <div class="box-header with-border bg-maroon">
+                    <h3 class="box-title"><i class="fa fa-user-secret">&nbsp;</i>BFI-MALAY</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body" style="">
+                    <p>Big Five Inventory-10-Malay (BFI-Malay)</p>
+                    <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['bfi/index'], ['class' => 'small-box-footer']) ?>
+                </div>
+                <!-- /.box-body -->
+            </div>
+        </div>
+        <div class="col-md-6 col-xs-12 col-lg-4">
+            <div class="box box-primary box-solid">
+                <div class="box-header with-border bg-purple">
+                    <h3 class="box-title"><i class="fa fa-street-view">&nbsp;</i>SDTS-PU</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body" style="">
+                    <p>Skala Daya Tindak Stres-Pelajar Universiti (SDTS-PU)</p>
+                    <?= Html::a('Jawab Soal Selidik <i class="fa fa-arrow-circle-right"></i>', ['bfi/index'], ['class' => 'small-box-footer']) ?>
+                </div>
+                <!-- /.box-body -->
+            </div>
+        </div>
+
 
 
     </div>
-    <!-- /.box -->
 </div>
