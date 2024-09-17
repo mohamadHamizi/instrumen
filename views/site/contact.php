b64cc0bf27e2f8257e40f9c09bc722177201a560
hamizi <hamizi@ums.edu.my>
2024-09-18 07:27:36 +0800

    change contact info

--- views/site/contact.php
+++ views/site/contact.php
@@ -15,19 +15,20 @@ use app\models\OkuRefDemo;
         <p>
             <strong>PROF. MADYA DR. MUHAMMAD IDRIS BIN BULLARE @ BAHARI</strong><br>
             <i>B.Psy., M.Psy., PhD (UMS)</i><br>
-            PENSYARAH KANAN
+            PROFESOR MADYA
         </p>
+
+
         <hr>
         <P>
-            UNIT PENYELIDIKAN PSIKOLOGI DAN KESIHATAN SOSIAL<br>
-            FAKULTI PSIKOLOGI DAN PENDIDIKAN<br>
+            FAKULTI PSIKOLOGI DAN KERJA SOSIAL<br>
             UNIVERSITI MALAYSIA SABAH, JALAN UMS<br>
             88400 KOTA KINABALU, SABAH, MALAYSIA<br><br>
             Tel: <strong>+088-320000</strong> Samb. <strong>8979</strong><br>
             Bimbit: <strong>+016-5202070</strong> Faks: <strong>+6088-320440</strong><br>
             E-mel: <strong>ferlis@ums.edu.my;ferlis27@yahoo.com</strong>
         </P>
-    </div>
+    </div>ßß
 
     <!-- /.box-body -->
     <div class="box-footer text-center">
